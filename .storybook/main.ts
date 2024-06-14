3603f417f25d96cbe321e70fd79751759eb475c4
Han-Na-05-22 <gkssktkfkd5@naver.com>
2024-06-14 13:37:14 +0900

    setting: storybook setting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,12 +6,22 @@ const config: StorybookConfig = {
 		'@storybook/addon-onboarding',
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
+		'@storybook/addon-styling',
 		'@chromatic-com/storybook',
 		'@storybook/addon-interactions',
 	],
+	core: {
+		builder: '@storybook/builder-vite',
+	},
 	framework: {
 		name: '@storybook/react-vite',
 		options: {},
+	},
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+	},
+	async viteFinal(config, options) {
+		return config;
 	},
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,23 @@
 import type { Preview } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/assets/styles/theme'; // 테마 파일 경로
+import { GlobalStyle } from '../src/assets/styles/global';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 
+export const decorators = [
+	withThemeFromJSXProvider({
+		themes: {
+			light: theme,
+			dark: theme,
+		},
+		defaultTheme: 'light',
+		Provider: ThemeProvider,
+		GlobalStyles: GlobalStyle,
+	}),
+];
 const preview: Preview = {
 	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
