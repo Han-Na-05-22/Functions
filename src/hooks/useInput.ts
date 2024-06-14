5feaff58c1e115566a9e02ae2a4432f08b8fe3df
Han-Na-05-22 <gkssktkfkd5@naver.com>
2024-06-14 13:39:55 +0900

    feat: useInput hooks 개발(input text, select input)

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,64 @@
+import { useCallback, useState } from 'react';
+
+type initValueType = string | number | boolean | object | any[];
+
+const useInputs = <T extends initValueType>(initValue: T) => {
+	const [inputValue, setInputValue] = useState<T>(initValue);
+
+	console.log('inputValue', inputValue);
+
+	const onChangeInputValue = useCallback(
+		(
+			e:
+				| React.ChangeEvent<HTMLInputElement>
+				| React.ChangeEvent<HTMLTextAreaElement>,
+		) => {
+			if (!e.target.value) {
+				return;
+			}
+
+			if (typeof initValue === 'string' || typeof initValue === 'number') {
+				setInputValue(e.target.value as T);
+			}
+
+			if (typeof initValue === 'object') {
+				const { name, value } = e.target;
+				setInputValue(
+					(prev: T) =>
+						({
+							...(prev as object),
+							[name]: value,
+						}) as T,
+				);
+			}
+
+			if (Array.isArray(initValue)) {
+				setInputValue((prev: T) => [...(prev as any[]), e.target.value] as T);
+			}
+		},
+		[],
+	);
+
+	const onChangeSelect = useCallback(
+		(e: React.ChangeEvent<HTMLSelectElement>) => {
+			const { name } = e.target;
+
+			const selectedValue = e.target.options[e.target.selectedIndex].value;
+
+			setInputValue((prev: T) => ({
+				...(prev as any),
+				[name]: selectedValue,
+			}));
+		},
+		[],
+	);
+
+	return {
+		inputValue,
+		setInputValue,
+		onChangeInputValue,
+		onChangeSelect,
+	};
+};
+
+export default useInputs;
