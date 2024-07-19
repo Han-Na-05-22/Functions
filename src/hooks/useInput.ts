198b2de23fea69991febf30ff65c431d4ac22230
Han-Na-05-22 <gkssktkfkd5@naver.com>
2024-07-19 12:49:30 +0900

    feat: add useinputs checkobox event

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,8 +1,15 @@
 import { RadioListProps } from 'components/common/RadioButton/interface';
 import { useCallback, useState } from 'react';
 
-type initValueType = string | number | boolean | object | any[];
+type initValueType =
+	| string
+	| number
+	| boolean
+	| object
+	| { [key: string]: any }
+	| any[];
 
+// input, select box, radio button, checkbox 관련 커스텀 훅
 const useInputs = <T extends initValueType>(initValue: T) => {
 	const [inputValue, setInputValue] = useState<T>(initValue);
 
@@ -83,12 +90,78 @@ const useInputs = <T extends initValueType>(initValue: T) => {
 		[initValue, inputValue],
 	);
 
+	const onChangeCheckBox = (
+		e: React.ChangeEvent<HTMLInputElement>,
+		checkListName?: string,
+		type?: string,
+	) => {
+		if (!e) {
+			return;
+		}
+
+		const { name, checked } = e.target;
+
+		setInputValue(
+			(prev: T) =>
+				({
+					...(prev as object),
+					[name]: checked,
+				}) as T,
+		);
+
+		if (!type && checkListName) {
+			const checkList = (inputValue as { [key: string]: any })[checkListName];
+
+			if (Array.isArray(checkList)) {
+				setInputValue(
+					(prev: T) =>
+						({
+							...(prev as object),
+							[checkListName]: checkList.map((i: any) => {
+								if (name === i.name) {
+									return { ...i, checked };
+								}
+								return i;
+							}),
+						}) as T,
+				);
+			} else if (typeof checkList === 'object') {
+				setInputValue(
+					(prev: T) =>
+						({
+							...(prev as object),
+							[checkListName]: {
+								...checkList,
+								[name]: checked,
+							},
+						}) as T,
+				);
+			}
+		}
+
+		if (type === 'all' && checkListName) {
+			setInputValue(
+				(prev: T) =>
+					({
+						...(prev as object),
+						[checkListName]: (prev as { [key: string]: any })[
+							checkListName
+						].map((check: { [key: string]: any }) => ({
+							...check,
+							checked,
+						})),
+					}) as T,
+			);
+		}
+	};
+
 	return {
 		inputValue,
 		setInputValue,
 		onChangeInputValue,
 		onChangeSelect,
 		onChangeRadioButton,
+		onChangeCheckBox,
 	};
 };
 
